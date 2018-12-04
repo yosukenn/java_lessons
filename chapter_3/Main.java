d82094058f115d76ad5c362b36103ec694aacfec
yo-tanaka2 <yo-tanaka2@dev-adminnoMacBook-puro-7.local>
2018-12-04 16:06:50 +0900

    fix <>

--- chapter_3/Main.java
+++ chapter_3/Main.java
@@ -6,9 +6,10 @@ public class Main {
     // 勇者２名をHeroとしてインスタンス化してArrayListに格納
     Hero h1 = new Hero("斎藤");
     Hero h2 = new Hero("鈴木");
-    List<Hero> heros = new ArrayList<Hero>();
-    heros.add(h1);
-    heros.add(h2);
+    List<Hero> heros = List.of(h1, h2);
+    // List<Hero> heros = new ArrayList<>();
+    // heros.add(h1);
+    // heros.add(h2);
     // １つずつ順番に取り出して名前を表示する
     Iterator<Hero> it = heros.iterator();
     while(it.hasNext()){
@@ -18,7 +19,7 @@ public class Main {
 
     // 3 - 3
     // それぞれの勇者が倒した敵の数(3, 7)を勇者とペアでコレクションに格納
-    Map<Hero, Integer> killboard = new HashMap<Hero, Integer>();
+    Map<Hero, Integer> killboard = new HashMap<>();
     killboard.put(h1, 3);
     killboard.put(h2, 7);
     // １つずつ取り出し、指定のように画面表示してください
