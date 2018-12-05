40b5b92afa44d80ac2bf9ae620c671ee24da029d
yo-tanaka2 <yo-tanaka2@dev-adminnoMacBook-puro-7.local>
2018-12-05 17:51:14 +0900

    rename Set instance

--- chapter_4/Main.java
+++ chapter_4/Main.java
@@ -4,30 +4,30 @@ import java.time.*;
 public class Main {
   public static void main(String[] args) {
     // インスタンスの等価判定の検証
-    Set<Book> list1 = new HashSet<Book>();
+    Set<Book> set = new HashSet<Book>();
     LocalDate now = LocalDate.now();
     Book b1 = new Book("桃太郎", now, "桃である。");
-    list1.add(b1);  // ここでb1の参照先のBookインスタンスがHashSetに格納される
-    System.out.println("要素数=" + list1.size());
+    set.add(b1);  // ここでb1の参照先のBookインスタンスがHashSetに格納される
+    System.out.println("要素数=" + set.size()); // 1
     b1 = new Book("桃太郎", now, "桃である。");  // b1を等価で更新（参照先は変わる）
-    list1.remove(b1);                         // b1の参照先のインスタンスと等価なものを削除 
-    System.out.println("要素数=" + list1.size());
+    set.remove(b1);                         // b1の参照先のインスタンスと等価なものを削除
+    System.out.println("要素数=" + set.size());
 
     // インスタンスの順序づけの検証
-    List<Book> list2 = new ArrayList<>();
+    List<Book> list = new ArrayList<>();
     Book book1 = new Book("浦島太郎", now.plusDays(10), "裏である。");
     Book book2 = new Book("金太郎", now.plusDays(40), "菌である");
     Book book3 = new Book("シンデレラ", now.plusDays(20), "シンである。");
-    list2.add(book1);
-    list2.add(book2);
-    list2.add(book3);
-    for (Book book : list2) {
+    list.add(book1);
+    list.add(book2);
+    list.add(book3);
+    for (Book book : list) {
       System.out.println(book.getTitle());
     }
     System.out.println("--------------------------");
 
-    Collections.sort(list2);
-    for (Book book : list2) {
+    Collections.sort(list);
+    for (Book book : list) {
       System.out.println(book.getTitle());
     }
 
