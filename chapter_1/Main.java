da465724fa17a9a011778296d3dbad8e71ccb6b3
yo-tanaka2 <yo-tanaka2@dev-adminnoMacBook-puro-7.local>
2018-12-06 11:18:45 +0900

    regular expression

--- chapter_1/Main.java
+++ chapter_1/Main.java
@@ -1,3 +1,5 @@
+import java.util.regex.Pattern;
+
 public class Main {
   // 1 - 2
   static String fileNameFormatter(String folder, String file) {
@@ -21,12 +23,28 @@ public class Main {
     String[] a = s.split(",");
 
     System.out.println(fileNameFormatter("/format", "readme.txt"));
+
+    // 1 - 3
+    // (1) 全ての文字列 .*
+    String str1 = "dmalfmlsa";
+    String pattern1 = ".*";
+    Pattern p1 = Pattern.compile(pattern1);
+    boolean isFound1 = p1.matcher(str1).find();
+    System.out.println(isFound1);
+
+    // (2) 最初の１文字はA、2文字目は数字、３文字目は数字か無し A¥d{1, 2} A[0-9]{1,2}
+    String str2 = "A022";
+    String pattern2 = "A¥d{1,2}";
+    Pattern p2 = Pattern.compile(pattern2);
+    boolean isFound2 = p2.matcher(str2).find();
+    System.out.println(isFound2);
+
+    // (3) 最初の１文字はU、2~4文字目は英大文字 U[A-Z]{3}
+    String str3 = "UJSK";
+    String pattern3 = "U[A-Z]{3}";
+    Pattern p3 = Pattern.compile(pattern3);
+    boolean isFound3 = p3.matcher(str3).find();
+    System.out.println(isFound3);
   }
 
-  // 1 - 3
-  // (1) 全ての文字列 .*
-  // (2) 最初の１文字はA、2文字目は数字、３文字目は数字か無し A¥d{1, 2} A[0-9]{1,2}
-  // (3) 最初の１文字はU、2~4文字目は英大文字 U[A-Z]{3}
-
-  
 }
