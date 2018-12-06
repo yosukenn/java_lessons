d2758455423bd85381569fe73eca9e424508f3c8
yo-tanaka2 <yo-tanaka2@dev-adminnoMacBook-puro-7.local>
2018-12-06 11:32:17 +0900

    StrongBox instance into variable and output

--- chapter_5/Main.java
+++ chapter_5/Main.java
@@ -2,6 +2,7 @@ public class Main {
   public static void main(String[] args) {
     StrongBox<String> s = new StrongBox<>(KeyType.FINGER);
     s.put("こんにちは");
-    System.out.println(s.get());
+    String hello = s.get();
+    System.out.println(hello);
   }
 }
