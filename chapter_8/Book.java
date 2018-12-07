61b5eb4b61da04415b67db5b4c0d865d23e4c5fb
yo-tanaka2 <yo-tanaka2@dev-adminnoMacBook-Pro.local>
2018-12-07 17:54:34 +0900

    implement equals hashCode compareTo using common-lang

---
+++ chapter_8/Book.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import org.apache.commons.lang3.builder.*;
+
+public class Book implements Comparable<Book> {
+  private String title;
+  private Date publishDate;
+  private String comment;
+
+  public Book(String title, Date publishDate, String comment) {
+    this.title = title;
+    this.publishDate = publishDate;
+    this.comment = comment;
+  }
+
+  // 署名と発行日とコメントが同じであれば等価なものと判定され、かつ、HashSetなどに格納しても正しく利用できる
+  @Override
+  public boolean equals(Object o) {
+    return EqualsBuilder.reflectionEquals(this, o);
+  }
+  @Override
+  public int hashCode() {
+    return HashCodeBuilder.reflectionHashCode(this);
+  }
+  // Bookインスタンスを格納したコレクションに対して、「Collections.sort();」を呼び出すと、発行日が古い順に並び替えられる
+  @Override
+  public int compareTo(Book o) {
+    return CompareToBuilder.reflectionCompare(this, o);
+  }
+  // clone()を呼び出すと、深いコピーによる複製が行われる
+}
---
+++ chapter_8/Main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Book b1 = new Book("桃太郎", new Date(), "昔々");
+    Book b2 = new Book("浦島太郎", new Date(), "昔々");
+  }
+}
