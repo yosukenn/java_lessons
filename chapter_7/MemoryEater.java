e5f048c88e3a73e2ebabc61690c0bf22882b9c16
yo-tanaka2 <yo-tanaka2@dev-adminnoMacBook-puro-7.local>
2018-12-07 11:58:20 +0900

    cretae MemoryEater

---
+++ chapter_7/MemoryEater.java
@@ -0,0 +1,9 @@
+public class MemoryEater {
+  public static void main(String[] args) {
+    System.out.println("要素数1280000のlong型配列を確保します。");
+    long[] eaterArray = new long[1280000];
+    for (int i = 0; i < eaterArray.length; i++) {
+      eaterArray[i] = i;
+    }
+  }
+}
