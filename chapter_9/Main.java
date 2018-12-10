97f0dc8a897fd7d2926da269db660d1aab9a8f1f
yo-tanaka2 <yo-tanaka2@dev-adminnoMacBook-Pro.local>
2018-12-10 17:28:24 +0900

    consider exception $ use GZIP / BufferedOutputStream

--- chapter_9/Main.java
+++ chapter_9/Main.java
@@ -1,24 +1,45 @@
 // 練習問題
 import java.io.*;
+import java.util.zip.GZIPOutputStream;
 
 // 起動コマンド
 // $ java Main [コピー元ファイルパス] [コピー先ファイルパス]
 public class Main {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     String inFile = args[0];
     String outFile = args[1];
 
-    FileInputStream fis = new FileInputStream(inFile);
-    FileOutputStream fos = new FileOutputStream(outFile);
+    FileInputStream fis = null;
+    FileOutputStream fos = null;
+    GZIPOutputStream gos = null;
+    BufferedOutputStream bos = null;
+    try {
+      fis = new FileInputStream(inFile);
+      fos = new FileOutputStream(outFile);
 
-    int i = fis.read();
-    while ( i != -1 ) {
-      char c = (char) i;
-      fos.write(c);
-      i = fis.read();
+      gos = new GZIPOutputStream(fos);
+      bos = new BufferedOutputStream(gos);
+
+      int i = fis.read();
+      while ( i != -1 ) {
+        bos.write(i);
+        i = fis.read();
+      }
+      fos.flush();
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    } finally {
+      try {
+        if (fis != null) {
+          fis.close();
+        }
+        if (fos != null) {
+          fos.close();
+        }
+      } catch (IOException ee) {
+
+      }
     }
-    fos.flush();
-    fos.close();
-    fis.close();
+
   }
 }
