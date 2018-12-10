fdae6f2ce79c1a92a93a1eedc18213a14399dd70
yo-tanaka2 <yo-tanaka2@dev-adminnoMacBook-puro-7.local>
2018-12-10 10:23:31 +0900

    not write public abstract in interface

--- chapter_6/Func1.java
+++ chapter_6/Func1.java
@@ -1,4 +1,4 @@
 // 関数を代入するためのインターフェースを用意
 public interface Func1 {
-  public abstract boolean judge(int x);
+  boolean judge(int x);
 }
--- chapter_6/Func2.java
+++ chapter_6/Func2.java
@@ -1,4 +1,4 @@
 // 関数を代入するためのインターフェースを用意
 public interface Func2 {
-  public abstract String addPrefix(boolean x, String y);
+  String addPrefix(boolean x, String y);
 }
