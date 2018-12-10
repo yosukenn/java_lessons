7f63d910fc69bb721ba26140325b0e80b0472d47
yo-tanaka2 <yo-tanaka2@dev-adminnoMacBook-Pro.local>
2018-12-10 19:05:55 +0900

    solve 10_1

--- chapter_10/Main.java
+++ chapter_10/Main.java
@@ -1,5 +1,29 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
   public static void main(String[] args) {
-    
+    // 10-1
+    Reader fr = null;
+    try {
+      // プロパティファイル形式のデータを読み込む
+      fr = new FileReader("pref.properties");
+      Properties p = new Properties();
+      p.load(fr);
+      // aichi.capitalとaichi.foodの内容を「名古屋：味噌カツ」の形式で画面に表示する
+      String aichiCapital = p.getProperty("aichi.capital");
+      String aichiFood = p.getProperty("aichi.food");
+      System.out.println(aichiCapital + " : " + aichiFood);
+    } catch(IOException e) {
+      System.out.println(e.getMessage());
+    }  finally {
+      try {
+        if (fr != null) {
+          fr.close();
+        }
+      } catch (IOException ee) {
+        
+      }
+    }
   }
 }
