27c4bccbb52abed7227d58adf62fd1c87e5d811b
yo-tanaka2 <yo-tanaka2@dev-adminnoMacBook-Pro.local>
2018-12-10 16:34:05 +0900

    try exception

--- chapter_9/TextSample.java
+++ chapter_9/TextSample.java
@@ -3,23 +3,43 @@ import java.io.*;
 public class TextSample {
   public static void main(String[] args) throws IOException {
     // ファイルへの書き込み
-    FileWriter fw = new FileWriter("rpgsave.txt", true);
+    FileWriter fw = null;
+    try {
+      fw = new FileWriter("rpgsave.txt", true);
 
-    fw.write('A');
-    fw.flush();
-    fw.close();
+      fw.write('A');
+      fw.flush();
+    } catch(IOException e) {
+      System.out.println("エラーです。");
+    } finally {
+      if (fw != null) {
+        try {
+          fw.close();   // close()は必ずfinallyブロックに書く
+        } catch(IOException e2) {}
+      }
+    }
 
     // ファイルの読み込み
-    FileReader fr = new FileReader("rpgsave.txt");
-    System.out.println("全てのデータを読んで表示します");
-    int i = fr.read();
-    while (i != -1) {
-      char c = (char) i;
-      System.out.println(c);
-      i = fr.read();
+    FileReader fr = null;
+    try {
+      fr = new FileReader("rpgsave.txt");
+      System.out.println("全てのデータを読んで表示します");
+      int i = fr.read();
+      while (i != -1) {
+        char c = (char) i;
+        System.out.println(c);
+        i = fr.read();
+      }
+      System.out.println("ファイルの末尾に到達しました。");
+    } catch(IOException e) {
+      System.out.println("エラーです。");
+    } finally {
+      if (fr != null) {
+        try {
+          fr.close();   // close()は必ずfinallyブロックに書く
+        } catch(IOException e2) {}
+      }
     }
-    System.out.println("ファイルの末尾に到達しました。");
-    fr.close();
 
   }
 }
