52a01688fa27d9e53bb76430197dc9c2e6fe41b5
yo-tanaka2 <yo-tanaka2@dev-adminnoMacBook-Pro.local>
2018-12-11 12:53:09 +0900

    add Socket communication

--- chapter_11/UrlSample.java
+++ chapter_11/UrlSample.java
@@ -1,12 +1,7 @@
 import java.io.*;
 import java.net.*;
 
-public class Main implements AutoCloseable {
-  @Override
-  public void close() throws IOException {
-      System.out.println("SampleFileReader Close.");
-  }
-
+public class UrlSample {
   public static void main(String[] args) {
     URL url = null;
 
