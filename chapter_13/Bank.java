40968d71cd84f3df64f08125c5f769e49ea83038
yo-tanaka2 <yo-tanaka2@dev-adminnoMacBook-Pro.local>
2018-12-13 11:16:43 +0900

    solve 13-1

---
+++ chapter_13/Bank.java
@@ -0,0 +1,23 @@
+package jp.miyabilink.atm;
+
+/**
+ * 銀行クラス。
+ * このクラスは、銀行の１支店を表します。
+ * @see 田中
+ */
+public class Bank {
+  /** 支店名 */
+  String name;
+  /** 支店住所 */
+  String address;
+
+  /**
+   * 口座開設を行うメソッド
+   * @param owner 口座開設依頼人
+   * @param initiialBalance 初期口座残高
+   */
+  public void addAccount(String owner, int initiialBalance) {}
+  public static void main(String[] args) {
+    System.out.println("試験用のメインメソッドです");
+  }
+}
