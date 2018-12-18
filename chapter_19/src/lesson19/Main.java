9cca7c23a545f7564e4193bec8690ae7a4d0b57a
yo-tanaka2 <yo-tanaka2@dev-adminnoMacBook-Pro.local>
2018-12-18 12:38:25 +0900

    fix lock object

--- chapter_19/src/lesson19/Main.java
+++ chapter_19/src/lesson19/Main.java
@@ -2,9 +2,11 @@ package lesson19;
 
 // 19 - 1
 class CountUpThread extends Thread {
+	private static Object lockObj = new Object();
+
 	public void run() {
 		// 「0 ~ 50の整数を順に表示する」[
-		synchronized(this) {
+		synchronized(lockObj) {
 			for (int i = 0; i < 51; i++) {
 				System.out.println(i);
 			}
@@ -17,9 +19,9 @@ public class Main {
 		runThread();
 		runThread();
 		runThread();
-		
+
 	}
-	
+
 	public static synchronized void runThread() {
 		Thread t = new CountUpThread();
 		t.start();
